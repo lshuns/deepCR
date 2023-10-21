23b319ecacd598696a702a29db44fd7686d1ad9d
lshuns <1006682997@qq.com>
2023-10-21 14:30:53 +0200

    replace with the original v0.2.0 version and add comments

--- deepCR/training.py
+++ deepCR/training.py
@@ -12,91 +12,97 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.tensorboard import SummaryWriter
 
 from deepCR.util import maskMetric
-from deepCR.dataset import dataset, DatasetSim
+from deepCR.dataset import dataset
 from deepCR.unet import WrappedModel, UNet2Sigmoid
 
 __all__ = 'train'
 
+class train():
 
-class VoidLRScheduler:
-    def __init__(self):
-        pass
+    def __init__(self, image, mask, 
+                    ignore=None, sky=None, aug_sky=[0, 0], 
+                    name='model', 
+                    hidden=32, gpu=False, epoch=50, batch_size=16, 
+                    lr=0.005, auto_lr_decay=True, lr_decay_patience=4, lr_decay_factor=0.1, 
+                    save_after=1e5, plot_every=10, 
+                    verbose=True, use_tqdm=False, use_tqdm_notebook=False, directory='./'):
+        """ 
+            Train deepCR-mask model.
 
-    def _reset(self):
-        pass
+        Parameters
+        ----------
+        image : np.ndarray (N*W*W)
+            Training data: image array with CR.
+        mask : np.ndarray (N*W*W)
+            Training data: CR mask array.
+        ignore : np.ndarray (N*W*W), optional
+            Training data: Mask for taking loss. e.g., bad pixel, saturation, etc.
+        sky : np.ndarray (N,), optional
+            Sky background
+        aug_sky : [float, float], optional
+            If sky is provided, use random sky background in the range
+            [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers 
+            to allow the trained model to adapt to a wider range of sky background 
+            or equivalently exposure time. Remedy the fact that exposure time in the
+            training set is discrete and limited.
+        name : str, optional 
+            Model name, model saved as name_epoch.pth
+        hidden : int, optional 
+            Number of channels for the first convolution layer.
+        gpu : bool, optional
+            To use GPU or not.
+        epoch : int, optional
+            Number of epochs to train.
+        batch_size : int, optional 
+            How many samples per batch to load.
+        lr : float, optional
+            Learning rate. 
+        auto_lr_decay : bool, optional
+            Reduce learning rate by "lr_decay_factor" after validation loss do not decrease for
+            "lr_decay_patience" + 1 epochs.
+        lr_decay_patience : float, optional
+            Reduce learning rate by lr_decay_factor after validation loss do not decrease for
+            "lr_decay_patience" + 1 epochs.
+        lr_decay_factor : float, optional
+            Factor by which the learning rate will be reduced. new_lr = lr * factor. 
+        save_after : float, optional
+            Epoch after which trainer automatically saves model state with lowest validation loss.
+        plot_every : float, optional
+            For every "plot_every" epoch, plot mask prediction and ground truth for 1st image in
+            validation set.
+        verbose : bool, optional
+            Print validation loss and detection rates for every epoch.
+        use_tqdm : bool, optional
+            Whether to show tqdm progress bar.
+        use_tqdm_notebook : bool, optional
+            Whether to use jupyter notebook version of tqdm. Overwrites tqdm_default.
+        directory : str, optional
+            Directory to save trained model.
 
-    def step(self):
-        pass
-
-
-class train:
-
-    def __init__(self, image, mask, ignore=None, sky=None, aug_sky=(0, 0), aug_img=(1, 1), noise=False, saturation=1e5,
-                 n_mask_train=1, n_mask_val=1, norm=False, percentile_limit=50, name='model', hidden=32, epoch=50,
-                 epoch_phase0=None, batch_size=16, lr=0.005, auto_lr_decay=True, lr_decay_patience=4,
-                 lr_decay_factor=0.1, save_after=1e5, plot_every=10, verbose=True, use_tqdm=False,
-                 use_tqdm_notebook=False, directory='./'):
-
-        """ This is the class for training deepCR-mask.
-        :param image: np.ndarray (N*W*W) training data: image array with CR.
-        :param mask: np.ndarray (N*W*W) training data: CR mask array
-        :param ignore: training data: Mask for taking loss. e.g., bad pixel, saturation, etc.
-        :param sky: np.ndarray (N,) (optional) sky background
-        :param aug_sky: [float, float]. If sky is provided, use random sky background in the range
-          [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
-          wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
-          training set is discrete and limited.
-        :param n_mask: number of dark images to sample to create one cosmic ray image and mask
-        :param name: model name. model saved to name_epoch.pth
-        :param hidden: number of channels for the first convolution layer. default: 50
-        :param gpu: True if use GPU for training
-        :param epoch: Number of epochs to train. default: 50
-        :param batch_size: training batch size. default: 16
-        :param lr: learning rate. default: 0.005
-        :param auto_lr_decay: reduce learning rate by "lr_decay_factor" after validation loss do not decrease for
-          "lr_decay_patience" + 1 epochs.
-        :param lr_decay_patience: reduce learning rate by lr_decay_factor after validation loss do not decrease for
-          "lr_decay_patience" + 1 epochs.
-        :param lr_decay_factor: multiplicative factor by which to reduce learning rate.
-        :param save_after: epoch after which trainer automatically saves model state with lowest validation loss
-        :param plot_every: for every "plot_every" epoch, plot mask prediction and ground truth for 1st image in
-          validation set.
-        :param verbose: print validation loss and detection rates for every epoch.
-        :param use_tqdm: whether to show tqdm progress bar.
-        :param use_tqdm_notebook: whether to use jupyter notebook version of tqdm. Overwrites tqdm_default.
-        :param directory: directory relative to current path to save trained model.
+        Returns
+        -------
+        None
         """
-        if torch.cuda.is_available():
-            gpu = True
-        else:
-            gpu = False
-            print('No GPU detected on this device! Training on CPU.')
 
-        if sky is None and aug_sky != (0, 0):
+        # check sky and ignore map
+        if sky is None and aug_sky != [0, 0]:
             raise AttributeError('Var (sky) is required for sky background augmentation!')
         if ignore is None:
             ignore = np.zeros_like(image)
-        if type(image) == np.ndarray and len(image.shape) == 3:
-            assert image.shape == mask.shape == ignore.shape
-            assert image.shape[1] == image.shape[2]
-            data_train = dataset(image, mask, ignore, sky, part='train', aug_sky=aug_sky)
-            data_val = dataset(image, mask, ignore, sky, part='val', aug_sky=aug_sky)
-        elif type(image[0]) == str:
-            data_train = DatasetSim(image, mask, sky, aug_sky=aug_sky, aug_img=aug_img, saturation=saturation,
-                                    norm=norm, percentile_limit=percentile_limit, part='train', noise=noise, n_mask=n_mask_train)
-            data_val = DatasetSim(image, mask, sky, aug_sky=aug_sky, aug_img=aug_img, saturation=saturation,
-                                  norm=norm, percentile_limit=percentile_limit, part='val', noise=noise, n_mask=n_mask_val)
-        else:
-            raise TypeError('Input must be numpy data arrays or list of file paths!')
+        assert image.shape == mask.shape == ignore.shape
+        assert image.shape[1] == image.shape[2]
 
-        self.TrainLoader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=8)
-        self.ValLoader = DataLoader(data_val, batch_size=batch_size, shuffle=False, num_workers=8)
-        self.shape = data_train[0][0].shape[1]
+        # create torch dataset
+        data_train = dataset(image, mask, ignore, sky, part='train', aug_sky=aug_sky)
+        data_val = dataset(image, mask, ignore, sky, part='val', aug_sky=aug_sky)
+        self.TrainLoader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=1)
+        self.ValLoader = DataLoader(data_val, batch_size=batch_size, shuffle=False, num_workers=1)
+        self.shape = image.shape[1]
         self.name = name
 
+        # use gpu or cpu
         if gpu:
             self.dtype = torch.cuda.FloatTensor
             self.dint = torch.cuda.ByteTensor
@@ -112,19 +118,14 @@ class train:
         if auto_lr_decay:
             self.lr_scheduler = ReduceLROnPlateau(self.optimizer, factor=lr_decay_factor, patience=lr_decay_patience,
                                                   cooldown=2, verbose=True, threshold=0.005)
-
         else:
-            self.lr_scheduler = VoidLRScheduler()
-        self.lr = lr
+            self.lr_scheduler = self._void_lr_scheduler
+
         self.BCELoss = nn.BCELoss()
         self.validation_loss = []
         self.epoch_mask = 0
         self.save_after = save_after
         self.n_epochs = epoch
-        if epoch_phase0 is None:
-            self.n_epochs_phase0 = int(self.n_epochs * 0.4 + 0.5)
-        else:
-            self.n_epochs_phase0 = epoch_phase0
         self.every = plot_every
         self.directory = directory
         self.verbose = verbose
@@ -135,9 +136,6 @@ class train:
         else:
             self.tqdm = tqdm
         self.disable_tqdm = not (use_tqdm_notebook or use_tqdm)
-
-        # tensorboard to record and visualize training log (require pytorch 1.1 and up)
-        self.writer = SummaryWriter(log_dir=directory)
 
     def set_input(self, img0, mask, ignore):
         """
@@ -150,12 +148,14 @@ class train:
         self.mask = Variable(mask.type(self.dtype)).view(-1, 1, self.shape, self.shape)
         self.ignore = Variable(ignore.type(self.dtype)).view(-1, 1, self.shape, self.shape)
 
-    def validate_mask(self, epoch=None):
+    @staticmethod
+    def _void_lr_scheduler(self, metric):
+        pass
+
+    def validate_mask(self):
         """
         :return: validation loss. print TPR and FPR at threshold = 0.5.
         """
-        torch.random.manual_seed(0)
-        np.random.seed(0)
         lmask = 0; count = 0
         metric = np.zeros(4)
         for i, dat in enumerate(self.ValLoader):
@@ -172,11 +172,6 @@ class train:
         FPR = FP / (FP + TN)
         if self.verbose:
             print('[TPR=%.3f, FPR=%.3f] @threshold = 0.5' % (TPR, FPR))
-        if epoch:
-            self.writer.add_scalar('TPR', TPR, epoch)
-            self.writer.add_scalar('FPR', FPR, epoch)
-            self.writer.add_scalar('validate_loss', lmask, epoch)
-
         return (lmask)
 
     def train(self):
@@ -184,22 +179,22 @@ class train:
         :return: None
         """
         if self.verbose:
-            print('Begin first {} epochs of training'.format(self.n_epochs_phase0))
-            print('Use batch statistics for batch normalization; keep running statistics to be used in phase1')
+            print('Begin first {} epochs of training'.format(int(self.n_epochs * 0.4 + 0.5)))
+            print('Use batch activate statistics for batch normalization; keep running mean to be used after '
+                  'these epochs')
             print('')
-        self.train_phase0(self.n_epochs_phase0)
+        self.train_initial(int(self.n_epochs * 0.4 + 0.5))
 
         filename = self.save()
         self.load(filename)
-        self.optimizer = optim.Adam(self.network.parameters(), lr=self.lr/2.5)
-
+        self.set_to_eval()
         if self.verbose:
-            print('Continue onto next {} epochs of training'.format(self.n_epochs - self.n_epochs_phase0))
+            print('Continue onto next {} epochs of training'.format(self.n_epochs - int(self.n_epochs * 0.4 + 0.5)))
             print('Batch normalization running statistics frozen and used')
             print('')
-        self.train_phase1(self.n_epochs - self.n_epochs_phase0)
+        self.train_continue(self.n_epochs - int(self.n_epochs * 0.4 + 0.5))
 
-    def train_phase0(self, epochs):
+    def train_initial(self, epochs):
         self.network.train()
         for epoch in self.tqdm(range(epochs), disable=self.disable_tqdm):
             for t, dat in enumerate(self.TrainLoader):
@@ -210,8 +205,8 @@ class train:
                 self.plot_example()
 
             if self.verbose:
-                print('----------- epoch = %d -----------' % self.epoch_mask)
-            val_loss = self.validate_mask(epoch)
+                print('----------- epoch = %d -----------' % (self.epoch_mask))
+            val_loss = self.validate_mask()
             self.validation_loss.append(val_loss)
             if self.verbose:
                 print('loss = %.4f' % (self.validation_loss[-1]))
@@ -224,9 +219,7 @@ class train:
             if self.verbose:
                 print('')
 
-    def train_phase1(self, epochs):
-        self.set_to_eval()
-        self.lr_scheduler._reset()
+    def train_continue(self, epochs):
         for epoch in self.tqdm(range(epochs), disable=self.disable_tqdm):
             for t, dat in enumerate(self.TrainLoader):
                 self.optimize_network(dat)
@@ -237,7 +230,7 @@ class train:
 
             if self.verbose:
                 print('----------- epoch = %d -----------' % self.epoch_mask)
-            valLossMask = self.validate_mask(epoch)
+            valLossMask = self.validate_mask()
             self.validation_loss.append(valLossMask)
             if self.verbose:
                 print('loss = %.4f' % (self.validation_loss[-1]))
@@ -307,3 +300,5 @@ class train:
         self.network.load_state_dict(torch.load(self.directory + filename + '.pth'))
         loc = filename.find('epoch') + 5
         self.epoch_mask = int(filename[loc:])
+
+
