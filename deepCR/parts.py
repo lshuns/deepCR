2abb3458cbfd3c6827ff31cbae7927539201489f
lshuns <1006682997@qq.com>
2023-10-23 14:09:44 +0200

    add some comments for understanding

--- deepCR/parts.py
+++ deepCR/parts.py
@@ -3,14 +3,23 @@ import torch.nn as nn
 
 
 class double_conv(nn.Module):
+    """
+    A double convolution block
+    """
     def __init__(self, in_ch, out_ch):
         super(type(self), self).__init__()
         self.conv = nn.Sequential(
+            # 3x3 convolution
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            # Batch normalisation
             nn.BatchNorm2d(out_ch, momentum=0.005),
+            # ReLU activation
             nn.ReLU(inplace=True),
+            # 3x3 convolution
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            # Batch normalisation
             nn.BatchNorm2d(out_ch, momentum=0.005),
+            # ReLU activation
             nn.ReLU(inplace=True)
         )
 
@@ -20,6 +29,9 @@ class double_conv(nn.Module):
 
 
 class inconv(nn.Module):
+    """
+    The initial convolutional operation
+    """
     def __init__(self, in_ch, out_ch):
         super(type(self), self).__init__()
         self.conv = double_conv(in_ch, out_ch)
@@ -30,10 +42,15 @@ class inconv(nn.Module):
 
 
 class down(nn.Module):
+    """
+    The downsampling block
+    """
     def __init__(self, in_ch, out_ch):
         super(type(self), self).__init__()
         self.mpconv = nn.Sequential(
+            # Max pooling with a 2x2 kernel
             nn.MaxPool2d(2),
+            # Call the double convolution block
             double_conv(in_ch, out_ch)
         )
 
@@ -43,21 +60,31 @@ class down(nn.Module):
 
 
 class up(nn.Module):
+    """
+    The upsampling block
+    """
     def __init__(self, in_ch, out_ch):
         super(type(self), self).__init__()
+        # Transposed convolution
         self.up = nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2)
+        # Call the double convolution block
         self.conv = double_conv(in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
+        # Concatenate feature maps
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
 
 
 class outconv(nn.Module):
+    """
+    The final convolution block
+    """
     def __init__(self, in_ch, out_ch):
         super(type(self), self).__init__()
+        # 1x1 convolution
         self.conv = nn.Conv2d(in_ch, out_ch, 1)
 
     def forward(self, x):
