81d8f7220a317d5e7c6c00653881d1480c5539b2
lshuns <1006682997@qq.com>
2023-11-02 11:37:42 +0100

    better data handling, variable names and separate train_mask

--- deepCR/dataset.py
+++ deepCR/dataset.py
@@ -1,99 +1,70 @@
+import os
 import numpy as np
+
+import torch
 from torch.utils.data import Dataset
 
-
 class dataset(Dataset):
-    def __init__(self, image, mask, ignore=None, sky=None, aug_sky=[0, 0], part=None, f_val=0.1, seed=1):
+    def __init__(self, data_dir, image_file_list, mask_file_list, ignore_file_list=None, 
+                    sky_val_list=None, aug_sky=[0, 0], gpu=False):
         """ custom pytorch dataset class to load deepCR-mask training data
-        :param image: image with CR
-        :param mask: CR mask
-        :param ignore: loss mask, e.g., bad pixel, saturation, etc.
-        :param sky: (np.ndarray) [N,] sky background level
+        :param data_dir: path where all images, masks and ignores are saved.
+        :param image_file_list: A list of file names for images (file in .npy)
+        :param mask_file_list: A list of file names for masks (file in .npy)
+        :param ignore_file_list: A list of file names for loss masks, e.g., bad pixel, saturation, etc.
+        :param sky_val_list: (np.ndarray) [N,] sky background level
         :param aug_sky: [float, float]. If sky is provided, use random sky background in the range
           [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers to allow the trained model to adapt to a
           wider range of sky background or equivalently exposure time. Remedy the fact that exposure time in the
           training set is discrete and limited.
-        :param part: either 'train' or 'val'. split by 0.8, 0.2
-        :param f_val: percentage of dataset reserved as validation set.
-        :param seed: fix numpy random seed to seed, for reproducibility.
         """
 
-        np.random.seed(seed)
+        self.data_dir = data_dir
+        self.image_file_list = image_file_list
+        self.mask_file_list = mask_file_list
+        self.ignore_file_list = ignore_file_list
 
-        ## images are provided in numpy array
-        if (type(image) == np.ndarray):
-            self.Nima = image.shape[0]
-        ## images are provided in path list
-        else:
-            self.Nima = len(image)
-
-        # split to train and validation sets
-        assert f_val < 1 and f_val > 0
-        f_train = 1 - f_val
-
-        # determine sets for train and validation
-        if part == 'train':
-            s = np.s_[:int(self.Nima * f_train)]
-        elif part == 'val':
-            s = np.s_[int(self.Nima * f_train):]
-        else:
-            s = np.s_[0:]
-        # print(f'>>> Images for {part}: {s}')
-
-        # select train or validation sets
-        self.image = image[s]
-        # print('---- selected image', self.image)
-        del image
-        self.mask = mask[s]
-        del mask
-
-        ## update number of images
-        if (type(self.image) == np.ndarray):
-            self.Nima = self.image.shape[0]
-        ## images are provided in path list
-        else:
-            self.Nima = len(self.image)
-
-        if ignore is not None:
-            self.ignore = ignore[s]
-            del ignore
-        else:
-            self.ignore = None
-
-        if sky is not None:
-            self.sky = sky[s]
-            del sky
-        else:
-            self.sky = None
-
-        del s
+        self.sky_val_list = sky_val_list
         self.aug_sky = aug_sky
 
+        ## determine which device to use
+        if gpu:
+            self.device = "cuda"
+            self.dtype = torch.cuda.FloatTensor
+        else:
+            self.device = "cpu"
+            self.dtype = torch.FloatTensor
+
     def __len__(self):
-        return self.Nima
+        # assume each path contains one image
+        return len(self.image_file_list)
 
     def __getitem__(self, i):
 
         # get image
-        image = self.image[i]
-        if (type(image) == str):
-            image = np.load(image)
+        image_path = os.path.join(self.data_dir, self.image_file_list[i])
+        image = np.load(image_path)
+
+        shape_tmp = image.shape[-1]
 
         # get mask
-        mask = self.mask[i]
-        if (type(mask) == str):
-            mask = np.load(mask)
+        mask_path = os.path.join(self.data_dir, self.mask_file_list[i])
+        mask = np.load(mask_path)
 
         # add sky
-        if self.sky is not None:
-            image = image + (self.aug_sky[0] + np.random.rand() * (self.aug_sky[1] - self.aug_sky[0])) * self.sky[i]
+        if self.sky_val_list is not None:
+            image = image + (self.aug_sky[0] + np.random.rand() * (self.aug_sky[1] - self.aug_sky[0])) * self.sky_val_list[i]
 
         # the ignore map
-        if self.ignore is not None:
-            ignore = self.ignore[i]
-            if (type(ignore) == str):
-                ignore = np.load(ignore)
-            return image, mask, ignore
+        if self.ignore_file_list is not None:
+            ignore_path = os.path.join(self.data_dir, self.ignore_file_list[i])
+            ignore = np.load(ignore_path)
+
+            # expanding the dimension for convolution
+            return torch.from_numpy(image).to(self.device).type(self.dtype).view(1, shape_tmp, shape_tmp), \
+                    torch.from_numpy(mask).to(self.device).type(self.dtype).view(1, shape_tmp, shape_tmp), \
+                    torch.from_numpy(ignore).to(self.device).type(self.dtype).view(1, shape_tmp, shape_tmp)
         else:
-            return image, mask
+            return torch.from_numpy(image).to(self.device).type(self.dtype).view(1, shape_tmp, shape_tmp), \
+                    torch.from_numpy(mask).to(self.device).type(self.dtype).view(1, shape_tmp, shape_tmp)
         
--- deepCR/training.py
+++ deepCR/training.py
@@ -20,31 +20,46 @@ from deepCR.util import maskMetric
 from deepCR.dataset import dataset
 from deepCR.unet_ssli import UNet
 
-__all__ = 'train'
+__all__ = ['train_mask']
 
-class train():
+class train_mask():
 
-    def __init__(self, image, mask, 
-                    ignore=None, sky=None, aug_sky=[0, 0], 
+    def __init__(self, train_data_dir, train_image_files, train_mask_files, 
+                    validation_data_dir, validation_image_files, validation_mask_files,
+                    train_ignore_files=None, validation_ignore_files=None, 
+                    train_sky_val_list=None, validation_sky_val_list=None,
+                    aug_sky=[0, 0],
                     name='model', 
                     nChannel_in=1, nChannel_out=1, nChannel_hidden=32, 
                     nLayers_down=1, return_type='sigmoid',
                     gpu=False, epoch_train=20, epoch_evaluate=20, batch_size=1, 
                     lr=0.005, auto_lr_decay=True, lr_decay_patience=4, lr_decay_factor=0.1, 
-                    verbose=True, use_tqdm=True, use_tqdm_notebook=False, directory='./'):
+                    use_tqdm=True, use_tqdm_notebook=False, directory='./'):
         """ 
             Train deepCR-mask model.
 
         Parameters
         ----------
-        image : np.ndarray (N*W*W) or a list of pathes to the image files
-            Training data: images with CR.
-        mask : np.ndarray (N*W*W) or a list of pathes to the mask files
-            Training data: CR mask.
-        ignore : np.ndarray (N*W*W) or a list of pathes to the ignore files, optional
-            Training data: Mask for taking loss. e.g., bad pixel, saturation, etc.
-        sky : np.ndarray (N,), optional
-            Sky background
+        train_data_dir : str
+            Directory where all training data are saved
+        train_image_files : a list of str
+            A list of image file names for training
+        train_mask_files : a list of str
+            A list of mask file names for training
+        validation_data_dir : str
+            Directory where all validation data are saved
+        validation_image_files : a list of str
+            A list of image file names for validation
+        validation_mask_files : a list of str
+            A list of mask file names for validation
+        train_ignore_files : a list of str, optional
+            A list of file names for mask of bad pixel, saturation, etc.
+        validation_ignore_files : a list of str, optional
+            A list of file names for mask of bad pixel, saturation, etc.
+        train_sky_val_list : np.ndarray (N,), optional
+            Sky background for training 
+        validation_sky_val_list : np.ndarray (N,), optional
+            Sky background for validation
         aug_sky : [float, float], optional
             If sky is provided, use random sky background in the range
             [aug_sky[0] * sky, aug_sky[1] * sky]. This serves as a regularizers 
@@ -81,8 +96,6 @@ class train():
             "lr_decay_patience" + 1 epochs.
         lr_decay_factor : float, optional
             Factor by which the learning rate will be reduced. new_lr = lr * factor. 
-        verbose : bool, optional
-            Print validation loss and detection rates for every epoch.
         use_tqdm : bool, optional
             Whether to show tqdm progress bar.
         use_tqdm_notebook : bool, optional
@@ -95,62 +108,59 @@ class train():
         None
         """
 
-        # check sky map
-        if sky is None and aug_sky != [0, 0]:
-            raise AttributeError('Var (sky) is required for sky background augmentation!')
+        # check images and masks
+        for data_dir, image_files, mask_files in ((train_data_dir, train_image_files, train_mask_files), 
+                                                (validation_data_dir, validation_image_files, validation_mask_files)):
+            ## number of images and masks should match
+            assert (len(image_files) == len(mask_files))
+            ## shape of image and mask should match
+            ima_tmp = np.load(os.path.join(data_dir, image_files[0]))
+            mask_tmp = np.load(os.path.join(data_dir, mask_files[0]))
+            del image_files, mask_files
+            assert (ima_tmp.shape == mask_tmp.shape)
+            del mask_tmp
+            ## image should be square
+            assert (ima_tmp.shape[0] == ima_tmp.shape[1])
+            del ima_tmp
 
-        ## images are provided in numpy array
-        if (type(image) == np.ndarray) and (type(mask) == np.ndarray):
-            # image and mask should match
-            assert image.shape == mask.shape
-            # image should be square
-            assert image.shape[1] == image.shape[2]
-            # save image size info
-            self.shape = image.shape[1]
-        ## images are provided in path list
-        elif (type(image[0]) == str) and (type(mask[0]) == str):
-            # image and mask should match
-            assert len(image) == len(mask)
-            ima_tmp = np.load(image[0])
-            mask_tmp = np.load(mask[0])
-            assert ima_tmp.shape == mask_tmp.shape
-            # image should be square
-            assert ima_tmp.shape[0] == ima_tmp.shape[1]
-            # save image size info
-            self.shape = ima_tmp.shape[0]
-            del ima_tmp, mask_tmp
+        # if ignore map is used
+        if (train_ignore_files is not None):
+            self.ignore_map = True
         else:
-            raise TypeError('Input image and mask must be numpy data arrays or list of file paths!')
+            self.ignore_map = False
 
         # create torch dataset
-        data_train = dataset(image, mask, ignore, sky, part='train', aug_sky=aug_sky)
-        data_val = dataset(image, mask, ignore, sky, part='val', aug_sky=aug_sky)
-        del image, mask, ignore, sky
+        data_train = dataset(train_data_dir, train_image_files, train_mask_files, train_ignore_files, 
+                    train_sky_val_list, aug_sky, gpu)
+        data_val = dataset(validation_data_dir, validation_image_files, validation_mask_files, validation_ignore_files, 
+                    validation_sky_val_list, aug_sky, gpu)
+        del train_data_dir, train_image_files, train_mask_files, train_ignore_files, train_sky_val_list, aug_sky
+        del validation_data_dir, validation_image_files, validation_mask_files, validation_ignore_files, validation_sky_val_list
 
         # create torch iterator
-        self.TrainLoader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=0)
+        self.TrainLoader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
         self.Ntrain = len(self.TrainLoader.dataset)
         print(f'>>> Number images for training: {self.Ntrain}')
-        self.ValLoader = DataLoader(data_val, batch_size=batch_size, shuffle=False, num_workers=0)
+        self.ValLoader = DataLoader(data_val, batch_size=batch_size, shuffle=False)
         print(f'>>> Number images for validation: {len(self.ValLoader.dataset)}')
         del data_train, data_val
 
+        # the name tag for saving
         self.name = name
 
         # initialise the network
+        ## determine which device to use
         if gpu:
-            self.dtype = torch.cuda.FloatTensor
-            self.dint = torch.cuda.ByteTensor
-            self.network = nn.DataParallel(
-                                UNet(nChannel_in, nChannel_out, nChannel_hidden, 
-                                    nLayers_down, return_type))
-            self.network.type(self.dtype)
+            device = "cuda"
         else:
-            self.dtype = torch.FloatTensor
-            self.dint = torch.ByteTensor
-            self.network = UNet(nChannel_in, nChannel_out, nChannel_hidden, 
-                                nLayers_down, return_type)
-            self.network.type(self.dtype)
+            device = "cpu"
+        print(f">>> Using {device} for training")
+        ## initialise
+        self.network = UNet(nChannel_in, nChannel_out, nChannel_hidden, 
+                                nLayers_down, return_type).to(device)
+        ## save parameter info
+        self.network_config = [nChannel_in, nChannel_out, nChannel_hidden, 
+                                nLayers_down, return_type]
 
         # initialise the optimizer
         self.optimizer = optim.Adam(self.network.parameters(), lr=lr)
@@ -166,38 +176,18 @@ class train():
         self.loss_fn = nn.BCELoss()
 
         self.validation_loss = []
-        self.lr = lr
 
         self.epoch_mask = 0
         self.n_epochs_train = epoch_train
         self.n_epochs_eva = epoch_evaluate
 
         self.directory = directory
-        self.verbose = verbose
 
         if use_tqdm_notebook:
             self.tqdm = tqdm_notebook
         else:
             self.tqdm = tqdm
         self.disable_tqdm = not (use_tqdm_notebook or use_tqdm)
-
-    def set_input(self, img0, mask, ignore=None):
-        """
-        :param img0: input image
-        :param mask: CR mask
-        :param ignore: loss mask
-        :return: None
-        """
-        self.img0 = Variable(img0.type(self.dtype)).view(-1, 1, self.shape, self.shape)
-        del img0
-        self.mask = Variable(mask.type(self.dtype)).view(-1, 1, self.shape, self.shape)
-        del mask
-
-        if ignore is not None:
-            self.ignore = Variable(ignore.type(self.dtype)).view(-1, 1, self.shape, self.shape)
-            del ignore
-        else:
-            self.ignore = None
 
     @staticmethod
     def _void_lr_scheduler(self, metric):
@@ -216,37 +206,46 @@ class train():
         # to reduce unnecessary gradient computations
         with torch.no_grad():
             for i, dat in enumerate(self.ValLoader):
+
+                # number of samples per batch
                 n = dat[0].shape[0]
                 count += n
 
-                # prepare the data
-                self.set_input(*dat)
-                del dat
-                # make prediction
-                pdt_mask = self.network(self.img0)
-                del self.img0
-                # compute loss
-                if self.ignore is not None:
-                    loss = self.loss_fn(pdt_mask * (1 - self.ignore), self.mask * (1 - self.ignore))
+                # get data
+                if self.ignore_map:
+                    images, masks, ignores = dat[0], dat[1], dat[2]
                 else:
-                    loss = self.loss_fn(pdt_mask, self.mask)
-                del self.ignore
+                    images, masks = dat[0], dat[1]
+                    ignores = None
+                del dat
+
+                # predict the mask
+                pdt_masks = self.network(images)
+                del images
+
+                # compute the loss
+                if ignores is not None:
+                    loss = self.loss_fn(pdt_masks * (1 - ignores), masks * (1 - ignores))
+                else:
+                    loss = self.loss_fn(pdt_masks, masks)                    
+                del ignores
+
                 # sum up loss
-                lmask += float(loss.detach()) * n
+                lmask += float(loss) * n
                 del loss
 
-                # calculate matrix
-                metric += maskMetric(pdt_mask.reshape(-1, self.shape, self.shape).detach().cpu().numpy() > 0.5, 
-                                     self.mask.reshape(-1, self.shape, self.shape).detach().cpu().numpy())
-                del pdt_mask, self.mask
+                # calculate confusion matrix
+                metric += maskMetric(pdt_masks.cpu().numpy() > 0.5, masks.cpu().numpy())
+                del pdt_masks, masks
 
-            lmask /= count
-            TP, TN, FP, FN = metric[0], metric[1], metric[2], metric[3]
-            TPR = TP / (TP + FN)
-            FPR = FP / (FP + TN)
+        lmask /= count
+        TP, TN, FP, FN = metric[0], metric[1], metric[2], metric[3]
+        del metric
+        TPR = TP / (TP + FN)
+        FPR = FP / (FP + TN)
+        print('Validation [TPR=%.3f, FPR=%.3f] @threshold = 0.5' % (TPR, FPR))
+        print('Validation loss = %.4f' % (lmask))
 
-        if self.verbose:
-            print('validation [TPR=%.3f, FPR=%.3f] @threshold = 0.5' % (TPR, FPR))
         return (lmask)
 
     def train(self, resume=False):
@@ -279,9 +278,8 @@ class train():
             self.lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
             self.validation_loss = checkpoint['validation_loss']
             del checkpoint
-            if self.verbose:
-                print('>>> Resume for {} epoch'.format(self.epoch_mask))
-                print('>>> Checkpoint loaded from {}'.format(os.path.basename(checkpoint_file)))
+            print('XXX Resume for {} epoch'.format(self.epoch_mask))
+            print('XXX Checkpoint loaded from {}'.format(os.path.basename(checkpoint_file)))
 
             if self.epoch_mask < self.n_epochs_train:
                 self._train(self.n_epochs_train - self.epoch_mask, mode='training')
@@ -290,17 +288,15 @@ class train():
                 self._train(self.n_epochs_eva + self.n_epochs_train - self.epoch_mask, mode='evaluation')
 
         else:
-            if self.verbose:
-                print('Begin first {} epochs for training mode'.format(self.n_epochs_train))
-                print('Use batch activate statistics for batch normalization; keep running mean to be used after '
-                      'these epochs')
-                print('')
+            print('Begin first {} epochs for training mode'.format(self.n_epochs_train))
+            print('Use batch activate statistics for batch normalization; keep running mean to be used after '
+                  'these epochs')
+            print('')
             self._train(self.n_epochs_train, mode='training')
 
-            if self.verbose:
-                print('Continue onto next {} epochs for evaluation mode'.format(self.n_epochs_eva))
-                print('Batch normalization running statistics frozen and used')
-                print('')
+            print('Continue onto next {} epochs for evaluation mode'.format(self.n_epochs_eva))
+            print('Batch normalization running statistics frozen and used')
+            print('')
             self._train(self.n_epochs_eva, mode='evaluation')
 
     def _train(self, epochs, mode='training'):
@@ -317,58 +313,61 @@ class train():
         for epoch in self.tqdm(range(epochs), disable=self.disable_tqdm):
 
             # running information
-            if self.verbose:
-                print(f'>>>>>>>>> start epoch ({mode}) {self.epoch_mask} ---------')
+            print(f'>>>>>>>>> start epoch ({mode}) {self.epoch_mask} ---------')
 
             # loop over all images and train for each epoch
             for t, dat in enumerate(self.TrainLoader):
 
-                # get the image data
-                self.set_input(*dat)
-                del dat
-                # predict the mask
-                pdt_mask = self.network(self.img0)
-                del self.img0
-                # compute the loss
-                if self.ignore is not None:
-                    loss = self.loss_fn(pdt_mask * (1 - self.ignore), self.mask * (1 - self.ignore))
+                # number of samples per batch
+                n = dat[0].shape[0]
+
+                # get data
+                if self.ignore_map:
+                    images, masks, ignores = dat[0], dat[1], dat[2]
                 else:
-                    loss = self.loss_fn(pdt_mask, self.mask)
-                del self.ignore, pdt_mask, self.mask
+                    images, masks = dat[0], dat[1]
+                    ignores = None
+                del dat
+
+                # predict the mask
+                pdt_masks = self.network(images)
+                del images
+
+                # compute the loss
+                if ignores is not None:
+                    loss = self.loss_fn(pdt_masks * (1 - ignores), masks * (1 - ignores))
+                else:
+                    loss = self.loss_fn(pdt_masks, masks)                    
+                del ignores, pdt_masks, masks
+
                 # backpropagation
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
                 # running information
-                if (self.verbose) and (t % 10 == 0):
-                    print(f'++++++ loss: {loss.item():>7f} [{t:>5d}/{self.Ntrain:>5d}]')
+                if (t % 10 == 0):
+                    loss, current = loss.item(), (t+1) * n
+                    print(f'++++++ loss: {loss:>7f} [{current:>5d}/{self.Ntrain:>5d}]')
 
             # collect the validation loss for each finished epoch
             val_loss = self.validate_mask()
             self.validation_loss.append(val_loss)
 
-            # Decay the learning rate is needed
+            # Decay the learning rate if needed
             self.lr_scheduler.step(self.validation_loss[-1])
-
-            # running information
-            if self.verbose:
-                print('validation loss = %.4f' % (self.validation_loss[-1]))
 
             ## save the model if the loss is improved
             if (np.array(self.validation_loss)[-1] == np.array(self.validation_loss).min()):
                 filename = self._save(mode=mode)
-                if self.verbose:
-                    print('Model saved to {}.pth'.format(filename))
+                print('Model saved to {}.pth'.format(filename))
 
             # save the checkpoint
             filename = self._save(mode='checkpoint')
-            if self.verbose:
-                print('Checkpoint saved to {}.checkpoint'.format(filename))
+            print('Checkpoint saved to {}.checkpoint'.format(filename))
 
             # running information
-            if self.verbose:
-                print(f'--------- finished epoch ({mode}) {self.epoch_mask} <<<<<<<<<')
+            print(f'--------- finished epoch ({mode}) {self.epoch_mask} <<<<<<<<<')
 
             # record the number of finished epoches
             self.epoch_mask += 1
@@ -382,7 +381,11 @@ class train():
         if mode != 'checkpoint':
             # saving model
             filename = f'{time}_{self.name}_{mode}_epoch{self.epoch_mask}'
-            torch.save(self.network.state_dict(), os.path.join(self.directory, filename + '.pth'))
+            torch.save({
+                        'network_config': self.network_config,
+                        'model_state_dict': self.network.state_dict()
+                        },
+                        os.path.join(self.directory, filename + '.pth'))
         else:
             # saving checkpoint
             filename = f'{time}_{self.name}_epoch{self.epoch_mask}'
